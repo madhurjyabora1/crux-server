13fae8513f154c4657c568eeda50142cf86b931a
Madhurjya Bora <madhurjya@csuitecircle.com>
2024-06-30 00:48:43 +0530

    cors

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { cruxReportRoutes } from "./routes/cruxReportRoutes.js";
 import cors from "cors";
-
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://crux-report-ui.vercel.app",
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use("/api", cruxReportRoutes);
 
 app.listen(3001, () => {
